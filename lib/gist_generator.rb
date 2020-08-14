de71166f5140d1b8b713a4fba5398982cf6965ea
Santiago Perez Perret <perezperret@gmail.com>
2020-08-14 13:31:05 -0300

    Initial commit

---
+++ lib/gist_generator.rb
@@ -0,0 +1,6 @@
+require "gist_generator/version"
+
+module GistGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gist_generator/version.rb
@@ -0,0 +1,3 @@
+module GistGenerator
+  VERSION = "0.1.0"
+end
